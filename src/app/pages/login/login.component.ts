24281e4c81a78d0567e3ba23f9fffd26b6f08bcd
Yuri Puodzius <yuri.puodzius@hotmail.com>
2025-07-09 16:15:42 +0200

    feat: add eventEmitter buttons

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,26 +1,50 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
-import { Form, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  Form,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+
+interface LoginForm {
+  email: FormControl,
+  password: FormControl
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    PrimaryInputComponent,
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  loginForm!: FormGroup  
+  loginForm!: FormGroup<LoginForm>;
 
-
-  constructor(){
+  constructor(
+    private router: Router
+  ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
-    })
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
 
+  submit(){
+    console.log(this.loginForm.value);
+  }
+  navigate() {
+    this.router.navigate(['/signup']);
+  }
 }
